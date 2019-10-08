d9fdd5ba6de8d822ea925282bb10c2fee022192f
cplank <cplankshepard@gmail.com>
2019-10-08 14:58:22 -0700

    change build folder to doc

---
+++ src/docs/precache-manifest.19fa59a9a4c59c796b367ed43e4cbdbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ee55a35c9f94ecd73fb6c8a433cff8",
+    "url": "/ticTacToe/index.html"
+  },
+  {
+    "revision": "acf421bffabce778f36b",
+    "url": "/ticTacToe/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8a134eca2e1a709fba6e",
+    "url": "/ticTacToe/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "acf421bffabce778f36b",
+    "url": "/ticTacToe/static/js/2.84a3b140.chunk.js"
+  },
+  {
+    "revision": "8a134eca2e1a709fba6e",
+    "url": "/ticTacToe/static/js/main.b7cf8c8e.chunk.js"
+  },
+  {
+    "revision": "f6719f728bdb642f0505",
+    "url": "/ticTacToe/static/js/runtime-main.1bfc1c12.js"
+  }
+]);
